67400b983d66c85e012ae6a0d31093e4331bec61
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2024-04-17 17:03:53 +0200

    Filter images to run in podman

--- run-javas-in-podman.sh
+++ run-javas-in-podman.sh
@@ -2,11 +2,16 @@
 
 set -ueo pipefail
 
+source ./version.rc
+
 indent() {
     sed 's#.*#    &#'
 }
 
-podman image ls --format '{{ .ID }} {{ .Repository }}:{{ .Tag }}' "$@" | while read image_id image_label; do
+podman image ls \
+    --format '{{ .ID }} {{ .Repository }}:{{ .Tag }}' \
+    "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION_TAG}-*" "$@" \
+| while read image_id image_label; do
     echo "$image_label [$image_id]"
     podman run --rm "$image_id" /bin/bash -c "java -version" 2>&1 | indent || true
 done
